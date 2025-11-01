489ee09debd4b72a809adc1526b75c488d3aeb89
Diego Jiménez <diegojmoravia@gmail.com>
2025-10-31 22:05:18 -0600

    feat: fetch pokemons from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [pokemons,setPokemons] = useState([]);
+
+  function fetchPokemons() {
+    axios
+      .get(`${process.env.REACT_APP_POKE_API_URL}?limit=492`)
+      .then((response) => {
+        const pokemons = response.data.results.map(e=> e.name);
+        setPokemons(pokemons)
+        })
+      .catch((error) => {
+        console.error("Error al hacer la petición:", error);
+      });
+  }
+
+  useEffect(() => {
+    fetchPokemons();
+  }, [])
+
   return (
-    <div className="App">
-      
+    <div>
+      <div>
+      </div>
     </div>
   );
 }
